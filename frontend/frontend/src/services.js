e32a3c1f73651623a78afa3a93f202027a469138
Lawrence Ezealor <ezealor20@gmail.com>
2024-04-18 17:51:56 +0100

    functionality

--- frontend/frontend/src/services.js
+++ frontend/frontend/src/services.js
@@ -12,14 +12,14 @@ export async function getCake() {
     const signer = await provider.getSigner();
     console.log("clicked");
     const contract = new ethers.Contract(
-      "0x9840b337fa8982354fc4aebb1e5524e0205f35c2",
+      "0xA53Efd344410Cca8a50cbf86FEbD8100EDCAdD60",
       contractABI,
       signer
     );
     console.log("clicked");
     try {
       console.log("clicked");
-      const cake = await contract.getCake();
+      const cake = await contract.getCakeOrder();
       console.log("clicked");
       console.log(cake);
       // console.log(transaction.wait)
@@ -36,14 +36,15 @@ export async function setCake(_color, _flavor, _size) {
     const provider = new ethers.BrowserProvider(window.ethereum);
     const signer = await provider.getSigner();
     const contract = new ethers.Contract(
-      "0x9840b337fa8982354fc4aebb1e5524e0205f35c2",
+      "0xA53Efd344410Cca8a50cbf86FEbD8100EDCAdD60",
       contractABI,
       signer
     );
     try {
-      const cakeDetails = await contract.setCake(_color, _flavor, _size);
+      const cakeDetails = await contract.placeCakeOrder(_color, _flavor, _size);
       cakeDetails.wait();
       console.log("Cake details set");
+      await getCake();
       return cakeDetails;
     } catch (err) {
       console.log(err);
